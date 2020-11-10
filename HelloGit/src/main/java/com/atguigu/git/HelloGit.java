262ceb2905cccc5df44f3702a776a9a4c5a861ab
wsming976 <647913860@qq.com>
2020-11-10 16:30:16 +0800

    第二次提交之前的添加

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -8,6 +8,7 @@ public class HelloGit {
         System.out.println("主干添加!");
         System.out.println("分支添加!");
         System.out.println("在线修改!");
+        System.out.println("第二次提交之前的添加!");
     }
 }
 
